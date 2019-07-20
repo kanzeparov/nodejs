d68e610759edef82007c00802dbd52cad725c997
Edgar Kaziakhmedov <edos.edgar@gmail.com>
2019-07-20 20:42:48 +0300

    Add mqtt client
    Signed-off-by: Edgar Kaziakhmedov <edos.edgar@gmail.com>

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ const Router = require('./router')
 let router = new Router()
 let app = express()
 let expressWs = require('express-ws')(app)
+let mqtt_cl = require('./mqtt_client')
+
+const mqtt = mqtt_cl.ClientMQTT()
+mqtt.add_handler(handler)
+mqtt.start()
+
+function handler(type, value) {
+  console.log("Receive new message %o", value)
+}
 
 app.ws('/', function(ws, req, next) {
   ws.on('message', function(msg) {
---
+++ js/mqtt_client.js
@@ -0,0 +1,70 @@
+import mqtt from 'mqtt'
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+class ClientMQTT {
+    constructor () {
+      super()
+      this.options = {
+          port: 8883,
+          host: "mqtt-stage.rnd.rtsoft.ru",
+          clientId: makeid(20),
+          username: "user1",
+          password: "jejcoilld7493",
+          keepalive: 60,
+          reconnectPeriod: 1000,
+          rejectUnauthorized: true,
+          protocol: 'mqtts'
+      }
+      this.started = 0
+      this.configuration = configuration
+    }
+  
+    topic_handler (topic, message) {
+      console.log("Received a new message from %o", topic.toString())
+      let json_msg = JSON.parse(message)
+      var msg_type = topic.split('/')[3]
+      if (msg_type == 'power' || msg_type == 'current' || msg_type == 'voltage') {
+        var power_value = {
+            // For example, enodeX
+            node: topic.split('/')[1],
+            // For ex, portX
+            port: topic.split('/')[2],
+            // power 
+            value: json_msg.value
+        }
+        this.handler(msg_type, power_value)
+      }
+    }
+  
+    connected () {
+      this.started = 1
+      console.log("Connected to the broker!")
+      this.Client.subscribe("/testbed/+/+/power")
+      this.Client.on('message', this.topic_handler.bind(this))
+    }
+
+    add_handler (handler) {
+      this.handler = handler
+    }
+  
+    start () {
+      console.log("Starting MQTT client")
+      this.Client = mqtt.connect(this.options)
+      this.Client.on('connect', this.connected.bind(this))
+    }
+  
+    stop () {
+      this.Client.end()
+    }
+  }
+
+  export {ClientMQTT}
